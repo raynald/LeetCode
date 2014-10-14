326afca48608866413da4d2e3eee7c159ac38b7a
Raynald Chung <raynaldchung@gmail.com>
2014-10-14 14:36:10 +0200

    sl

--- Sort_List.cc
+++ Sort_List.cc
@@ -1,129 +1,83 @@
 /**
- *
  *  * Definition for singly-linked list.
- *
  *   * struct ListNode {
- *
  *    *     int val;
- *
  *     *     ListNode *next;
- *
  *      *     ListNode(int x) : val(x), next(NULL) {}
- *
  *       * };
- *
  *        */
 
 class Solution {
-
     public:
+        ListNode *mergesort(ListNode *start, int n) {
+            cout << start-> val << " " << n<< endl;
+            if(n==1) return start;
+            int mid = n/2;
+            ListNode *right = start;
+            ListNode *p1, *p2, *np, *np2;
+            int l1=mid;
+            int l2=n-mid;
+            for(int i=1;i<=mid;i++) {
+                right = right->next;
+            }
+            start = mergesort(start, mid);
+            right = mergesort(right, n-mid);
+            if(start->val<right->val) {
+                np = start;
+                p1 = start->next;
+                p2 = right;
+                l1--;
+            }
+            else {
+                np = right;
+                p2 = right->next;
+                p1 = start;
+                l2--;
+            }
+            np2 = np;
+            while(l1!=0 && l2!=0) {
+                cout <<"loop1"<<endl;
+                if (p1->val<p2->val) {
+                    np -> next = p1;
+                    np = p1;
+                    p1 = p1 -> next;
+                    l1--;
+                }
+                else {
+                    np -> next = p2;
+                    np = p2;
+                    p2 = p2 -> next;
+                    l2--;
+                }
+            }
+            while(l1!=0) {
+                cout << p1->val << endl;
+                if(p1==p1->next) cout <<"fuck"<<endl;
+                cout <<"loop2"<<endl;
+                np -> next = p1;
+                np = p1;
+                p1 = p1 -> next;
+                l1--;
+            }
+            while(l2!=0) {
+                cout <<"loop3"<<endl;
+                np -> next = p2;
+                np = p2;
+                p2 = p2 -> next;
+                l2--;
+            }
+            np->next=NULL;
+            return np2;
+        }
 
-            ListNode *mergesort(ListNode *start, int n) {
-
-                        if(n==1) return start;
-
-                                int mid = n/2;
-
-                                        ListNode *right = start;
-
-                                                ListNode *p1, *p2, *np, *np2;
-
-                                                        for(int i=1;i<=mid;i++) {
-
-                                                                        right = right->next;
-
-                                                                                }
-
-                                                                mergesort(start, mid);
-
-                                                                        mergesort(right, n-mid);
-
-                                                                                if(start->val<right->val) {
-
-                                                                                                np = start;
-
-                                                                                                            p1 = start->next;
-
-                                                                                                                    }
-
-                                                                                        else {
-
-                                                                                                        np = right;
-
-                                                                                                                    p2 = right->next;
-
-                                                                                                                            }
-
-                                                                                                np2 = np;
-
-                                                                                                        while(p1!=NULL && p2!=NULL) {
-
-                                                                                                                        if (p1->val<p2->val) {
-
-                                                                                                                                            np -> next = p1;
-
-                                                                                                                                                            np = p1;
-
-                                                                                                                                                                            p1 = p1 -> next;
-
-                                                                                                                                                                                        }
-
-                                                                                                                                    else {
-
-                                                                                                                                                        np -> next = p2;
-
-                                                                                                                                                                        np = p2;
-
-                                                                                                                                                                                        p2 = p2 -> next;
-
-                                                                                                                                                                                                    }
-
-                                                                                                                                            }
-
-                                                                                                                while(p1!=NULL) {
-
-                                                                                                                                np -> next = p1;
-
-                                                                                                                                            np = p1;
-
-                                                                                                                                                        p1 = p1 -> next;
-
-                                                                                                                                                                }
-
-                                                                                                                        while(p2!=NULL) {
-
-                                                                                                                                        np -> next = p2;
-
-                                                                                                                                                    np = p2;
-
-                                                                                                                                                                p2 = p2 -> next;
-
-                                                                                                                                                                        }
-
-                                                                                                                                return np2;
-
-                                                                                                                                    }
-
-                
-
-                ListNode *sortList(ListNode *head) {
-
-                            if(head==NULL) return NULL;
-
-                                    int length = 0;
-
-                                            ListNode *p = head;
-
-                                                    while(p!=NULL) {
-
-                                                                    length ++;
-
-                                                                                p = p -> next;
-
-                                                                                        }
-
-                                                            return mergesort(head, length);
-
-                                                                }
-
+        ListNode *sortList(ListNode *head) {
+            if(head==NULL) return NULL;
+            int length = 0;
+            ListNode *p = head;
+            while(p!=NULL) {
+                length ++;
+                p = p -> next;
+            }
+            return mergesort(head, length);
+        }
 };
--- main.cc
+++ main.cc
@@ -1,17 +1,21 @@
 #include<iostream>
 #include"Sort_list.cc"
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x): val(x), next(NULL) {}
-};
-
 int main() {
-    ListNode *p1,*p2;
-    p1 = p2 = ListNode(2);
-    p2->next = ListNode(1);
+    ListNode p2(3);
+    ListNode *p1= &p2;
+    ListNode *head=p1;
+    ListNode p3(4);
+    p2.next = &p3;
+    ListNode p4(1);
+    p3.next = &p4;
     Solution sol;
+    p1 = head;
+    while(p1!=NULL) {
+        cout << p1->val << endl;
+        p1 = p1 ->next;
+    }
+    p1 = head;
     p1 = sol.sortList(p1);
     while(p1!=NULL) {
         cout << p1->val << endl;
